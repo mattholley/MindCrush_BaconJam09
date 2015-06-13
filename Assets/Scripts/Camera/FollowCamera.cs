cae29f921faf6411c6c1eb018d64281e40fddd21
Matt Holley <matt@mattholley.ca>
2015-06-13 02:04:41 -0400

    * Modified Camera parameters to use Ortho

--- Assets/Scripts/Camera/FollowCamera.cs
+++ Assets/Scripts/Camera/FollowCamera.cs
@@ -7,7 +7,7 @@ public class FollowCamera : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-	
+		m_camera = GetComponent<Camera>();
 	}
 	
 	// Update is called once per frame
@@ -17,7 +17,14 @@ public class FollowCamera : MonoBehaviour
 		{
 			transform.rotation = Quaternion.Euler(AXONOGRAPHIC_DIRECTION);
 			Vector3 cameraTarget = m_target.transform.position - transform.forward * m_followDistance;
+
 			transform.position = Vector3.SmoothDamp(transform.position, cameraTarget, ref m_velocity, m_smoothing);
+
+			//Should always be ortho, but why not
+			if(m_camera.orthographic)
+			{
+				m_camera.orthographicSize = m_followDistance;
+			}
 		}
 	}
 
@@ -25,6 +32,8 @@ public class FollowCamera : MonoBehaviour
 	public GameObject m_target;
 	public float m_followDistance = 10.0f;
 	public float m_smoothing = 1.0f;
+
+	private Camera m_camera;
 
 	private Vector3 m_velocity;
 
