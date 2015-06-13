601effbcc48f02c5190d4288522930feefd09109
Matt Holley <matt@mattholley.ca>
2015-06-13 16:21:18 -0400

    + Added a sensory system allowing for different triggers to inform the AI of the player presence, made a visual sensor, and an 'awareness' sensor. * Polished up the AI.

---
+++ Assets/Scripts/Character/AI/VisualSensor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisualSensor : Sensor
+{
+
+	// Use this for initialization
+	protected override void Start () 
+	{
+		base.Start();
+	}
+	
+	// Update is called once per frame
+	protected override void Update () 
+	{
+		base.Update();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		SensorResult result;
+		result.sensor = this;
+		result.obj = other.gameObject;
+		result.enter = true;
+		DispatchSensorResult(ref result);
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		SensorResult result;
+		result.sensor = this;
+		result.obj = other.gameObject;
+		result.enter = false;
+		DispatchSensorResult(ref result);
+	}
+}
