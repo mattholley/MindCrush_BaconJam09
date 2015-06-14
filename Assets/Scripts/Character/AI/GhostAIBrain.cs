589fbd471648128ebc4ddf2ca3c22a07fdb28bae
punyaan <bpleonvong@gmail.com>
2015-06-13 20:13:05 -0400

    Made Test Model with Animations

--- Assets/Scripts/Character/AI/GhostAIBrain.cs
+++ Assets/Scripts/Character/AI/GhostAIBrain.cs
@@ -4,43 +4,144 @@ using System.Collections.Generic;
 
 public class GhostAIBrain : AIBrain
 {
-
+    /*
     protected override void Start()
     {
         base.Start();
+
+        m_bIsActive = true;
+        m_strCAction = "Spawned";
+        m_Target = GameObject.Find("Player");
     }
 
     protected override void Update()
     {
         base.Update();
+
+        if (m_bIsActive)
+        {
+            switch (m_CState)
+            {
+                case State.IDLE:
+                    {
+                        m_strCAction = "Entering Idle";
+                        IdleState();
+                        break;
+                    }
+                case State.WANDER:
+                    {
+                        m_strCAction = "Entering Wander";
+                        WanderState();
+                        break;
+                    }
+                case State.COMBAT:
+                    {
+                        m_strCAction = "Entering Combat";
+                        CombatState();
+                        break;
+                    }
+                case State.DEAD:
+                    {
+                        m_strCAction = "Entering Dead";
+                        DeadState();
+                        break;
+                    }
+            }
+        }
+        else
+        {
+
+        }
         CheckHealth();
     }
 
-	protected override void IdleState()
+    void IdleState()
     {
-		base.IdleState();
+        if (m_nIdleTime > m_nIdleTimer)
+        {
+            m_CState = State.WANDER;
+            //m_CWaypoints.Add(new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5)));
+            m_nIdleTime = 0.0f;
+        }
+        else
+        {
+            m_nIdleTime += Time.deltaTime;
+        }
     }
 
-	protected override void WanderState()
+    void WanderState()
     {
-		base.WanderState();
+        if (m_CCurrentWaypoint < m_CWaypoints.Count)
+        {
+            //Get distance to waypoint
+            //If waypoint is within distance, go to next waypoint
+            //Else
+            //Get direction to waypoint
+            //Move in direction
+            //If colliding with obstacle and not at waypoint
+            //Jump
+
+            Vector3 target = m_CWaypoints[m_CCurrentWaypoint].position;
+            Vector3 distance = target - transform.position;
+
+            //If close enough to the waypoint, go to the next one
+            if (distance.magnitude < 0.4f)
+            {
+                transform.position = target;
+                ++m_CCurrentWaypoint;
+            }
+            else
+            {
+                //[Matt] This will need to be smoother
+                transform.LookAt(target);
+
+                Vector2 direction = new Vector2(distance.x, distance.z) * m_nSpeed;
+                m_characterBehavior.SetVelocity(ref direction);
+            }
+        }
+        else
+        {
+            m_CState = State.IDLE;
+            m_CCurrentWaypoint = 0;
+        }
+        CheckTargetDistance();
     }
 
-	protected override void CombatState()
+    void CombatState()
     {
-		base.CombatState();
+        Vector3 target = m_Target.transform.position;
+
+        //[Matt] This will need to be smoother
+        transform.LookAt(target);
+
+        Vector3 distance = target - transform.position;
+        Vector2 direction = new Vector2(distance.x, distance.z) * m_nSpeed;
+
+        m_characterBehavior.SetVelocity(ref direction);
+        CheckTargetDistance();
     }
 
-	protected override void DeadState()
+    void DeadState()
     {
-		base.DeadState();
+    }
+
+    void CheckTargetDistance()
+    {
+        if (Vector3.Distance(m_Target.transform.position, gameObject.transform.position) <= 15)
+        {
+            m_CState = State.COMBAT;
+        }
+        else
+        {
+            m_CState = State.WANDER;
+        }
     }
 
     void CheckHealth()
     {
-        if(m_health < 1)
+        if(m_nCHealth <1)
         {
-            SetState(State.DEAD);
+            m_CState = State.DEAD;
         }
     }
 
@@ -61,5 +162,6 @@ public class GhostAIBrain : AIBrain
 
     [Header("Ghost Specific Properties")]
     public GameObject m_Target;
-
+    public List<string> m_CSkills;
+    */
 }
